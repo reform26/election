f32cfff2ba555290e68aa1f54f6aa8ef84c10b7d
reform26 <pointrally9999@gmail.com>
2026-04-23 19:10:17 +0900

    Add files via upload

--- can.js
+++ can.js
@@ -3806,7 +3806,7 @@ const candidates = [
             "국민체육진흥공단 경륜선수",
             "현) 그린닥터스 경상남도 청년단장"
         ],
-        photo: "https://i.imgur.com/FNIPpKw.png",
+        photo: "https://i.imgur.com/3nEWaQQ.png",
         sns: {
             fb: "#",
             ig: "https://www.instagram.com/moonbest11/"
@@ -3867,8 +3867,8 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-23",
         coords: [
-            37.528145,
-            126.916286
+           37.528145,
+           126.916286
         ]
     },
     {
@@ -4244,7 +4244,7 @@ const candidates = [
         donation: "https://givemoney.kr/politicians/175/",
         dateAdded: "2026-04-20",
         coords: [
-            37.534,
+            37.5340,
             126.9886
         ]
     },
@@ -4340,7 +4340,7 @@ const candidates = [
         donation: "https://givemoney.kr/politicians/184",
         dateAdded: "2026-04-20",
         coords: [
-            35.136,
+            35.1360,
             126.7979
         ]
     },
@@ -4522,7 +4522,7 @@ const candidates = [
         dateAdded: "2026-04-23",
         coords: [
             37.543227,
-            126.87322
+            126.873220
         ]
     },
     {
@@ -4549,8 +4549,8 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-22",
         coords: [
-            37.3749,
-            126.729348
+           37.374900,
+           126.729348
         ]
     },
     {
@@ -4564,7 +4564,7 @@ const candidates = [
         status: "예비후보",
         bio: [
             "제8대 울산시의원",
-            "제6,7대 남구의회 의원",
+            "제6,7대 남구의회 의원", 
             "울산시의회의원 예산결산특별위원장"
         ],
         photo: "https://i.imgur.com/TbQkeLr.png",
@@ -4689,7 +4689,7 @@ const candidates = [
         dateAdded: "2026-04-23",
         coords: [
             35.535388,
-            129.32285
+            129.322850
         ]
     },
     {
@@ -4714,7 +4714,7 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-23",
         coords: [
-            35.49129,
+            35.491290,
             129.351269
         ]
     },
@@ -4740,7 +4740,7 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-23",
         coords: [
-            35.524458,
+            35.524458, 
             129.290415
         ]
     },
@@ -4773,7 +4773,7 @@ const candidates = [
             129.312083
         ]
     }
-];
+ ];
 
 
 
