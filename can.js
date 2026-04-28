4be83223b8e0115365419751bd0da2566657fbd9
reform26 <pointrally9999@gmail.com>
2026-04-28 14:01:42 +0900

    Add files via upload

--- can.js
+++ can.js
@@ -604,7 +604,7 @@ const candidates = [
         dateAdded: "2026-04-24",
         coords: [
             36.650683,
-            127.48825
+            127.488250
         ]
     },
     {
@@ -1662,7 +1662,7 @@ const candidates = [
         sns: {
             fb: "#",
             ig: "https://www.instagram.com/official_yongjun",
-            blog: "https://blog.naver.com/onedaylife97"
+            blog : "https://blog.naver.com/onedaylife97"
         },
         pledge: "#",
         decl: "https://rallypoint.kr/board/politics-free/224918",
@@ -2742,10 +2742,11 @@ const candidates = [
             "현) 풍경채 대표",
             "전) 국가철도공단 시설장비사무소 산업안전관리담당자"
         ],
-        photo: "",
+        photo: "https://i.imgur.com/yxkrZiM.png",
         district: "나",
         sns: {
-            fb: "#"
+            fb: "#",
+            ig: "https://www.instagram.com/jinho6472/"
         },
         pledge: "#",
         decl: "#",
@@ -2998,7 +2999,7 @@ const candidates = [
             fb: "https://www.facebook.com/profile.php?id=61576886771004",
             ig: "https://www.instagram.com/jminwoo712",
             yt: "https://www.youtube.com/@tomygod123",
-            blog: "https://blog.naver.com/jminwoo712"
+            blog : "https://blog.naver.com/jminwoo712"
         },
         pledge: "#",
         decl: "#",
@@ -3210,6 +3211,7 @@ const candidates = [
     {
         name: "박은강",
         region: "경기 과천시",
+        visibility: "hidden",
         metropolitan: "경기도",
         category: "기초의원",
         office: "과천시의회",
@@ -3337,7 +3339,7 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-24",
         coords: [
-            37.207775,
+            37.207775, 
             127.056313
         ]
     },
@@ -3918,8 +3920,8 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-23",
         coords: [
-            37.528145,
-            126.916286
+           37.528145,
+           126.916286
         ]
     },
     {
@@ -3992,7 +3994,7 @@ const candidates = [
         ],
         photo: "https://i.imgur.com/v1UfClC.png",
         sns: {
-            ig: "https://www.instagram.com/daniel_star_kim/"
+           ig: "https://www.instagram.com/daniel_star_kim/"
         },
         pledge: "#",
         decl: "#",
@@ -4295,7 +4297,7 @@ const candidates = [
         donation: "https://givemoney.kr/politicians/175/",
         dateAdded: "2026-04-20",
         coords: [
-            37.534,
+            37.5340,
             126.9886
         ]
     },
@@ -4326,6 +4328,7 @@ const candidates = [
     {
         name: "손승현",
         region: "서울 용산구",
+        visibility: "hidden",
         metropolitan: "서울특별시",
         category: "기초의원",
         office: "용산구의회",
@@ -4386,13 +4389,15 @@ const candidates = [
             "전)정부세종청사 이든샘어린이집 담임교사"
         ],
         photo: "https://i.imgur.com/OXhWgZh.png",
-        sns: {},
+        sns: {
+          ig : "https://www.instagram.com/mini_l.e.e/"
+          },
         pledge: "#",
         decl: "#",
         donation: "https://givemoney.kr/politicians/184",
         dateAdded: "2026-04-20",
         coords: [
-            35.136,
+            35.1360,
             126.7979
         ]
     },
@@ -4461,7 +4466,8 @@ const candidates = [
         ],
         photo: "https://i.imgur.com/sA6Zbmx.png",
         sns: {
-            fb: "https://www.facebook.com/keumran.go/"
+            fb: "https://www.facebook.com/keumran.go/",
+            blog : "https://blog.naver.com/mulan1997"
         },
         pledge: "#",
         decl: "#",
@@ -4632,7 +4638,7 @@ const candidates = [
         dateAdded: "2026-04-23",
         coords: [
             37.543227,
-            126.87322
+            126.873220
         ]
     },
     {
@@ -4659,8 +4665,8 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-22",
         coords: [
-            37.3749,
-            126.729348
+           37.374900,
+           126.729348
         ]
     },
     {
@@ -4676,7 +4682,7 @@ const candidates = [
         status: "예비후보",
         bio: [
             "<span style='letter-spacing:-0.12em;'>전) 패스트파이브 파이브스팟 서비스기획 총괄</span>",
-            "<span style='letter-spacing:-0.12em;'>전) 라운즈(ROUNZ) 서비스기획팀 팀장</span>"
+            "<span style='letter-spacing:-0.12em;'>전) 라운즈(ROUNZ) 서비스기획팀 팀장</span>",
         ],
         photo: "",
         sns: {
@@ -4687,8 +4693,8 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-27",
         coords: [
-            37.37491,
-            126.729349
+           37.374910,
+           126.729349
         ]
     },
     {
@@ -4702,7 +4708,7 @@ const candidates = [
         status: "예비후보",
         bio: [
             "제8대 울산시의원",
-            "제6,7대 남구의회 의원",
+            "제6,7대 남구의회 의원", 
             "울산시의회의원 예산결산특별위원장"
         ],
         photo: "https://i.imgur.com/TbQkeLr.png",
@@ -4833,7 +4839,7 @@ const candidates = [
         dateAdded: "2026-04-27",
         coords: [
             35.535388,
-            129.32285
+            129.322850
         ]
     },
     {
@@ -4860,7 +4866,7 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-27",
         coords: [
-            35.49129,
+            35.491290,
             129.351269
         ]
     },
@@ -4889,7 +4895,7 @@ const candidates = [
         donation: "#",
         dateAdded: "2026-04-27",
         coords: [
-            35.524458,
+            35.524458, 
             129.290415
         ]
     },
@@ -4922,7 +4928,7 @@ const candidates = [
             129.312083
         ]
     }
-];
+ ];
 
 
 
